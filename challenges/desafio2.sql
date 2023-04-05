1cd4f9de3d080832c7a4ecb7ad31a4d7a79dcf42
Matheus <matheussantos.quintanilha@gmail.com>
2023-04-05 11:52:15 -0300

    ajeitando o requisito 2

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,8 +1,7 @@
-
 SELECT
-    COUNT(DISTINCT c.musica_id) AS cancoes,
+    COUNT(DISTINCT Md.musica_id) AS cancoes,
     COUNT(DISTINCT Ar.artista_id) AS artistas,
     COUNT(DISTINCT Al.album_id) AS albuns
 FROM SpotifyClone.album Al
 JOIN SpotifyClone.artistas Ar ON Al.artista_id = Ar.artista_id
-JOIN SpotifyClone.musicas C ON Al.album_id = c.album_id
+JOIN SpotifyClone.musicas Md ON Al.album_id = Md.album_id
