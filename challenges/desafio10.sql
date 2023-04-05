b5bbb7ad3b93028fcf0e7526f22a29aaa3087083
Matheus <matheussantos.quintanilha@gmail.com>
2023-04-05 15:02:54 -0300

    finalizado requisito 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -11,3 +11,30 @@
 --   ('exemplo de dados 1', 'exemplo de dados A'),
 --   ('exemplo de dados 2', 'exemplo de dados B'),
 --   ('exemplo de dados 3', 'exemplo de dados C');
+USE SpotifyClone;
+
+DROP TABLE IF EXISTS SpotifyClone;
+
+CREATE TABLE favoritar_musica_das_pessoas(
+  usuario_id INT,
+  musica_id INT,
+    CONSTRAINT PRIMARY KEY(usuario_id, musica_id),
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+  FOREIGN KEY (musica_id) REFERENCES musicas(musica_id)
+) ENGINE = InnoDB;
+
+INSERT INTO favoritar_musica_das_pessoas (usuario_id, musica_id)
+VALUES
+    (1, 3),
+    (1, 6),
+    (1, 10),
+    (2, 4),
+    (3, 1),
+    (3, 3),
+    (4, 7),
+    (4, 4),
+    (5, 10),
+    (5, 2),
+    (8, 4),
+    (9, 7),
+    (10, 3);
